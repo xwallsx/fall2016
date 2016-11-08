cff6b58be3d620eb8636106a1b6b75386d27e7ec
Matt Walls <Matt Walls>
2016-11-08 14:07:57 -0500

    initial commit

--- code.cpp
+++ code.cpp
@@ -3,20 +3,20 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
+#include <time.h>
+using namespace std;
 
 int main(){
 	int n;
-	std::cout<<"Enter a number the is n^2. i.e. 4, 9, 16, 25, ect.\n";
-	std::cin>> n;
-	vector<vector<int> a(n,vector<int>(n));
+	cout<<"Enter a number the is n^2. i.e. 4, 9, 16, 25, ect.\n";
+	cin>> n;
+	vector<vector<int>> a(n,vector<int>(n));
 	srand(time(0));
 	for(int a_i = 0; a_i<n;a_i++)
 	{
 		for(int a_j=0;a_j<n;a_j++)
 		{
-			int x = rand() % 10+1;	
-			int y = rand() % 10+1;
-			cin >> a[x][y];
+			a[a_i][a_j]= rand() % 10+1;
 		}
 
 	}
@@ -27,7 +27,7 @@ int main(){
 		sum[1] = a[a_i][(n-1) -a_i];
 	}
 
-	std::cout << abs(sum[0]-sum[1]);
+	cout << abs(sum[0]-sum[1]);
 	return 0;
 }
 
